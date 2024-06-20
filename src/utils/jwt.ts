f247734b4d6a46418f6f2e9bf898f505d2d78681
maxi-gomez <maximiliano.gomez@mercadolibre.cl>
2024-06-20 16:40:12 -0400

    second commit

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,13 @@
-import jwt from 'jsonwebtoken'
-import Types from 'mongoose'
+import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
 
 type UserPayload = {
-    id: Types.ObjectId
-}
+    id: Types.ObjectId;
+};
 
 export const generateJWT = (payload: UserPayload) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
         expiresIn: '180d'
-    })
-    return token
-}
+    });
+    return token;
+};
